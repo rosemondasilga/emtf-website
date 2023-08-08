6d0df5fe719b98e9705088f0cfa44533aed2898c
tedashikode <theodoremawuli77@gmail.com>
2023-08-08 22:53:21 +0000

    Update CSS and JavaScript files with new styles and functionality

--- script.js
+++ script.js
@@ -3,61 +3,75 @@ console.log('testing...');
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const supabase = createClient('https://usfjmohkctzxjizmledb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzZmptb2hrY3R6eGppem1sZWRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEyNjQ3MDgsImV4cCI6MjAwNjg0MDcwOH0.Leblc7VH2rYfJTQKwFc3s6mON8-PbNLNzJrZvh94Tlg')
-// const supabaseUrl = process.env.SUPABASE_URL
-// const supabaseKey = process.env.SUPABASE_KEY
-// const mySupabase = createClient(supabaseUrl, supabaseKey)
 
-const fetchSupabase = async () => {
+async function fetchSupabase() {
     // getting data from the users table
     const { data, error } = await supabase
         .from('Users')
-        .select('*')
+        .select()
 
         console.log('fetching data');
         if (error) console.log('error', error)
         console.log('data', data)
 }
 
-console.log('Supabase Instance: ', supabase)
-fetchSupabase()
-
 const loginForm = document.querySelector('#login-form');
 const signupForm = document.querySelector('#signup-form');
+const searchForm = document.querySelector('#search-form');
+const searchResultsSection = document.querySelector('#search-results');
 
 const signupFormData = new FormData(signupForm);
+const searchFormData = new FormData(searchForm);
+
 const username = signupFormData.get('username');
 const email = signupFormData.get('email');
 const password = signupFormData.get('password');
 
-const loginFormData = new FormData(loginForm);
-const loginEmail = loginFormData.get('email');
-const loginPassword = loginFormData.get('password');
+const search = searchFormData.get('search');
+
+// const loginFormData = new FormData(loginForm);
+// const loginEmail = loginFormData.get('email');
+// const loginPassword = loginFormData.get('password');
 
 
-signupForm.addEventListener('submit', function (event) {
+signupForm.addEventListener('submit', async function (event) {
     event.preventDefault(); // Prevent the default form submission
 
     // Validate the form data (if needed)
-    if (!email || !password) {
-        alert('Please fill in all the fields.');
-        return;
-    }
+    // if (!email || !password) {
+    //     alert('Please fill in all the fields.');
+    //     return;
+    // }
 
     // Construct the data payload to send to Supabase
-    const data = {
+    const userData = {
         username: username,
         email: email,
         password: password,
     };
 
+    fetchSupabase()
+    console.log('Supabase Instance: ', supabase)
+
+    // Use Supabase's auth.signUp() method
+    let {data: Users, error} = await supabase.auth.signUp({ username, email, password })
+    .then(response => {
+        console.log('Sign-up successful:', response);
+        // Optionally, you can redirect the user to a login page after successful sign-up.
+    })
+    .catch(error => {
+        console.error('Sign-up error:', error);
+    }); 
+
     // Send the data to Supabase using fetch()
-    fetch('https://usfjmohkctzxjizmledb.supabase.co/Users', {
+    fetch(supabase, {
         method: 'POST',
         headers: {
+        'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
-        'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzZmptb2hrY3R6eGppem1sZWRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEyNjQ3MDgsImV4cCI6MjAwNjg0MDcwOH0.Leblc7VH2rYfJTQKwFc3s6mON8-PbNLNzJrZvh94Tlg',
+        // 'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzZmptb2hrY3R6eGppem1sZWRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEyNjQ3MDgsImV4cCI6MjAwNjg0MDcwOH0.Leblc7VH2rYfJTQKwFc3s6mON8-PbNLNzJrZvh94Tlg',
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(userData),
     })
     .then(response => response.json())
     .then(result => {
@@ -70,25 +84,24 @@ signupForm.addEventListener('submit', function (event) {
     .catch(error => {
         console.error('Error sending data:', error);
     });
-    
-    // Use Supabase's auth.signUp() method
-    
-    supabase.auth.signUp({ email, password })
-    .then(response => {
-        console.log('Sign-up successful:', response);
-        // Optionally, you can redirect the user to a login page after successful sign-up.
-    })
-    .catch(error => {
-        console.error('Sign-up error:', error);
-    });
+
+
+});
+
+loginForm.addEventListener('submit', async function (event) {
+    event.preventDefault(); // Prevent the default form submission
+    if (!email || !password) {
+        alert('Please fill in all the fields.');
+        return;
+    }
     
     // Use Supabase's auth.signIn() method
-    supabase.auth.signIn({ loginEmail, loginPassword })
+    let {data, error} = await supabase.auth.signInWithPassword({ email, password })
     .then(response => {
         console.log('Login successful:', response);
         // Optionally, you can redirect the user to a dashboard or protected page after successful login.
     })
     .catch(error => {
         console.error('Login error:', error);
-    });       
+    });     
 });
